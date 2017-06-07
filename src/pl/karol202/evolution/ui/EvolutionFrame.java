85f36afb56a3dca6f1f12ed2d66768df51e684b5
karol-202 <karoljurski1@gmail.com>
2017-06-08 01:25:35 +0200

    Add bottom panel and buttons for zooming.

--- src/pl/karol202/evolution/ui/EvolutionFrame.java
+++ src/pl/karol202/evolution/ui/EvolutionFrame.java
@@ -1,12 +1,16 @@
 package pl.karol202.evolution.ui;
 
+import pl.karol202.evolution.utils.ButtonHovering;
 import pl.karol202.evolution.world.World;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class EvolutionFrame extends JFrame
 {
 	private World world;
+	
+	private EvolutionPanel evolutionPanel;
 	
 	private JMenuBar menuBar;
 	
@@ -18,6 +22,11 @@ public class EvolutionFrame extends JFrame
 	private JRadioButtonMenuItem itemViewTemperature;
 	private JRadioButtonMenuItem itemViewHumidity;
 	
+	private JPanel panelBottom;
+	private JLabel labelScale;
+	private ButtonHovering buttonMinus;
+	private ButtonHovering buttonPlus;
+	
 	public EvolutionFrame(World world)
 	{
 		super("Evolution");
@@ -25,10 +34,11 @@ public class EvolutionFrame extends JFrame
 		
 		setSize(1024, 768);
 		setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		setLayout(new BorderLayout());
 		setVisible(true);
 
-		EvolutionPanel panel = new EvolutionPanel(world);
-		add(panel);
+		evolutionPanel = new EvolutionPanel(world);
+		add(evolutionPanel, BorderLayout.CENTER);
 		
 		menuBar = new JMenuBar();
 		setJMenuBar(menuBar);
@@ -46,14 +56,32 @@ public class EvolutionFrame extends JFrame
 		groupView = new ButtonGroup();
 		
 		itemViewTemperature = new JRadioButtonMenuItem("Temperatura");
-		itemViewTemperature.addActionListener(e -> panel.setViewMode(ViewMode.TEMPERATURE));
+		itemViewTemperature.addActionListener(e -> evolutionPanel.setViewMode(ViewMode.TEMPERATURE));
 		itemViewTemperature.setSelected(true);
 		groupView.add(itemViewTemperature);
 		menuView.add(itemViewTemperature);
 		
 		itemViewHumidity = new JRadioButtonMenuItem("Wilgotność");
-		itemViewHumidity.addActionListener(e -> panel.setViewMode(ViewMode.HUMIDITY));
+		itemViewHumidity.addActionListener(e -> evolutionPanel.setViewMode(ViewMode.HUMIDITY));
 		groupView.add(itemViewHumidity);
 		menuView.add(itemViewHumidity);
+		
+		panelBottom = new JPanel(new GridBagLayout());
+		add(panelBottom, BorderLayout.SOUTH);
+		
+		labelScale = new JLabel("100%");
+		panelBottom.add(labelScale, new GridBagConstraints(1, 0, 1, 1, 0, 0,
+				GridBagConstraints.EAST, GridBagConstraints.NONE, new Insets(0, 0, 0, 0),
+				0, 0));
+		
+		buttonMinus = new ButtonHovering("res/minus.png");
+		panelBottom.add(buttonMinus, new GridBagConstraints(0, 0, 1, 1, 1, 0,
+				GridBagConstraints.EAST, GridBagConstraints.NONE, new Insets(0, 0, 0, 0),
+				0, 0));
+		
+		buttonPlus = new ButtonHovering("res/plus.png");
+		panelBottom.add(buttonPlus, new GridBagConstraints(2, 0, 1, 1, 0, 0,
+				GridBagConstraints.CENTER, GridBagConstraints.NONE, new Insets(0, 0, 0, 0),
+				0, 0));
 	}
 }
---
+++ src/pl/karol202/evolution/utils/ButtonHovering.java
@@ -0,0 +1,84 @@
+package pl.karol202.evolution.utils;
+
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+public class ButtonHovering extends JPanel implements MouseListener
+{
+	private BufferedImage image;
+	private Color defaultColor;
+	private Color hoverColor;
+	private Color pressedColor;
+	private boolean pressed;
+	private boolean hover;
+	
+	public ButtonHovering(String path)
+	{
+		super();
+		setImage(path);
+		setPreferredSize(new Dimension(image.getWidth(), image.getHeight()));
+		addMouseListener(this);
+		
+		defaultColor = UIManager.getColor("Control");
+		hoverColor = new Color(216, 216, 216);
+		pressedColor = new Color(180, 180, 180);
+		pressed = false;
+		hover = false;
+	}
+	
+	@Override
+	public void mouseClicked(MouseEvent e) { }
+	
+	@Override
+	public void mousePressed(MouseEvent e)
+	{
+		setBackground(pressedColor);
+		pressed = true;
+	}
+	
+	@Override
+	public void mouseReleased(MouseEvent e)
+	{
+		setBackground(hover ? hoverColor : defaultColor);
+		pressed = false;
+	}
+	
+	@Override
+	public void mouseEntered(MouseEvent e)
+	{
+		setBackground(pressed ? pressedColor : hoverColor);
+		hover = true;
+	}
+	
+	@Override
+	public void mouseExited(MouseEvent e)
+	{
+		setBackground(defaultColor);
+		hover = false;
+	}
+	
+	private void setImage(String path)
+	{
+		try
+		{
+			image = ImageIO.read(new File(path));
+		}
+		catch(IOException ex)
+		{
+			ex.printStackTrace();
+		}
+	}
+	
+	@Override
+	protected void paintComponent(Graphics g)
+	{
+		super.paintComponent(g);
+		g.drawImage(image, 0, 0, image.getWidth(), image.getHeight(), null);
+	}
+}
